32e61d1ebd75c93b05971ede76310982911b6bb5
Rushil Sheth <rjsheth2@dons.usfca.edu>
2020-03-26 20:13:46 -0700

    first commit

---
+++ user_definition.py
@@ -0,0 +1,6 @@
+# https://developers.google.com/maps/documentation/javascript/get-api-key
+apikey = "AIzaSyAbd_8XudJ0XWLUd2Q3UdStty2oRZcWIWY"
+orig_coord = '37.7909,-122.3925'
+dest_coord = '37.7765,-122.4506'
+output_file = 'time.tsv'
+bucket_name="msds603-week1-rushil"
